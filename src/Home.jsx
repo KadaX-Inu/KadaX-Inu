35f1eced58b23f82b12bf9e3cbacc55c2f989a57
KadaX <shortreels2go@gmail.com>
2025-02-19 00:47:25 -0500

    Initial commit for KadaX-Inu website

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to KadaX Inu</h1>
+            <p>Your future crypto ecosystem.</p>
+        </div>
+    );
+}
+
+export default Home;
